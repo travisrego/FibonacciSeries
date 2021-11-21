d97b463b4e4b2af4da5fff0e5bc54daee920453d
Travis Rego <ms.anitarego@gmail.com>
2021-11-21 14:52:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Figgle;
+
+namespace FibonacciSeries
+{
+    internal static class Program
+    {
+        private static decimal _number1;
+        private static decimal _number2 = 1;
+        private static decimal _x;
+        private static FileStream _fontStream; 
+
+        private static void Main()
+        {
+            WindowUtility.MoveWindowToCenter();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            _fontStream = File.OpenRead(@"D:\Documents\Projects\code\Practice\Learning C Sharp\FibonacciSeries\assets\Ogre.flf");
+            var font = FiggleFontParser.Parse(_fontStream);
+            var text = font.Render("Fibonacci  Series");
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Enter the number of elements: ");
+            _x = Convert.ToDecimal(Console.ReadLine()); // 79228162514264337593543950335
+            var xIsSmall = false;
+            while (!xIsSmall)
+            {
+                if (_x <= 137)
+                {
+                    Console.Clear();
+
+                    Console.Write("Loading...");
+                    Thread.Sleep(1500);
+
+                    Console.Clear();
+            
+                    Console.WriteLine(string.Empty);
+                    Console.WriteLine(string.Empty);
+            
+                    Console.WriteLine("This is {0} elements of the Fibonacci Series", _x);
+                    Console.WriteLine(string.Empty);
+            
+                    Console.Write(_number1 + ", " + _number2);
+                    for (decimal i = 0; i <= _x; i++)
+                    {
+                        decimal number3 = _number1 + _number2;
+                        Console.Write(", {0}", number3);
+                        _number1 = _number2; // 1 
+                        _number2 = number3; // 1
+                    }
+                    
+                    RestartConsole();
+                    xIsSmall = true; 
+                }
+                else
+                {
+                    Console.Clear();
+                    WindowUtility.MoveWindowToCenter();
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    _fontStream = File.OpenRead(@"D:\Documents\Projects\code\Practice\Learning C Sharp\FibonacciSeries\assets\Ogre.flf");
+                    font = FiggleFontParser.Parse(_fontStream);
+                    text = font.Render("Fibonacci  Series");
+                    Console.WriteLine(text);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("Enter the number of elements: ");
+                    Console.ForegroundColor = ConsoleColor.Red; 
+                    Console.WriteLine("Your number is too big try a number equal or less than 137");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    _x = Convert.ToDecimal(Console.ReadLine()); // 79228162514264337593543950335
+                }
+            }
+        }
+
+        private static void RestartConsole()
+        {
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("Do you want to restart console?");
+            Console.WriteLine("Type 1 or Y - Yes");
+            Console.Write("Type 2 or N - No");
+            Console.WriteLine(string.Empty);
+            string input = Console.ReadLine()?.ToUpper();
+            while (true)
+            {
+                _number1 = 0;
+                _number2 = 1;
+                switch (input)
+                {
+                    case "1" or "Y":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Restarting...");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Thread.Sleep(1000);
+                        System.Diagnostics.Process.Start("FibonacciSeries.exe");
+                        Console.Clear();
+                        Environment.Exit(0);
+                        return;
+                    case "2" or "N":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Exiting console...");
+                        Thread.Sleep(1000);
+                        Environment.Exit(0);
+                        return;
+                }
+
+                Console.Clear();
+            
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(string.Empty);
+                Console.WriteLine("This is {0} elements of the Fibonacci Series", _x);
+                Console.WriteLine(string.Empty);
+                Console.Write(_number1 + ", " + _number2);
+                for (decimal i = 0; i <= _x; i++)
+                {
+                    decimal number3 = _number1 + _number2;
+                    Console.Write(", {0}", number3);
+                    _number1 = _number2; // 1 
+                    _number2 = number3; // 1
+                }
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(string.Empty);
+                Console.WriteLine("Do you want to restart console?");
+                
+                Console.WriteLine("Type 1 or Y - Yes");
+                Console.Write("Type 2 or N - No");
+                Console.WriteLine(string.Empty);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a valid option!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                input = Console.ReadLine()?.ToUpper();
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ WindowUtility.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace FibonacciSeries
+{
+    internal static class WindowUtility
+{
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+    private const uint SwpNosize = 0x0001;
+    private const uint SwpNozorder = 0x0004;
+
+    private static Size GetScreenSize() => new Size(GetSystemMetrics(0), GetSystemMetrics(1));
+
+    private readonly struct Size
+    {
+        public int Width { get; }
+        public int Height { get; }
+
+        public Size(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+
+    [DllImport("User32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+    private static extern int GetSystemMetrics(int nIndex);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool GetWindowRect(HandleRef hWnd, out Rect lpRect);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct Rect
+    {
+        public readonly int Left;        // x position of upper-left corner
+        public readonly int Top;         // y position of upper-left corner
+        public readonly int Right;       // x position of lower-right corner
+        public readonly int Bottom;      // y position of lower-right corner
+    }
+
+    private static Size GetWindowSize(IntPtr window)
+    {
+        if (!GetWindowRect(new HandleRef(null, window), out Rect rect))
+            throw new Exception("Unable to get window rect!");
+
+        int width = rect.Right - rect.Left;
+        int height = rect.Bottom - rect.Top;
+
+        return new Size(width, height);
+    }
+
+    public static void MoveWindowToCenter()
+    {
+        IntPtr window = Process.GetCurrentProcess().MainWindowHandle;
+
+            if (window == IntPtr.Zero)
+                return; 
+
+        Size screenSize = GetScreenSize();
+        Size windowSize = GetWindowSize(window);
+
+        int x = (screenSize.Width - windowSize.Width) / 2;
+        int y = (screenSize.Height - windowSize.Height) / 2;
+
+        SetWindowPos(window, IntPtr.Zero, x, y, 0, 0, SwpNosize | SwpNozorder);
+    }
+}}
